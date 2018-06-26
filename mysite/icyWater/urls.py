b8bb7c6f6414c8c335de04a70ca1ea482d0e7189
carrington cooper <carrington10@ksu.edu>
2018-06-25 17:29:32 -0700

    for the css html static files

--- mysite/icyWater/urls.py
+++ mysite/icyWater/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from .import views
+
 urlpatterns = [
 url(r'^$',views.home),
+url(r"^login/$",login,{'template_name':'account/login.html'}),
 
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/',include('icyWater.urls')),
+    url(r'^icywat/',include('icyWater.urls')),
 ]
