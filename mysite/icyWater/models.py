1b41020189955ce04b62c23e958edb9ef8e34a9f
carrington cooper <carrington10@ksu.edu>
2018-07-23 17:17:59 -0700

    this is for audio player

--- mysite/icyWater/models.py
+++ mysite/icyWater/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class MusicPost(models.Model):
+class Song(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     title = models.CharField(max_length=50,default="")
+    music=models.FileField(upload_to='isemusic',null=True)
     description =(models.TextField(max_length=100,default=""))
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
--- mysite/icyWater/views.py
+++ mysite/icyWater/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Song
 # Create your views here.
 
-def home(request):
-    return render(request,'home.html');
+class HomeView(TemplateView):
+      template_name="home.html"
+      def get(self,request):
+                    songs = Song.objects.all().order_by('-created')
+                    args = {
+                            'songs':songs
+                        }
+                    return render(request, self.template_name, args)
 
 def aboutme(request):
     return render(request,'aboutme.html');
